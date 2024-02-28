2cc373baff6049326caaeb8a9e3935504e20f954
ClaytonDewey <clay@dryadmedia.com>
2024-02-28 15:39:09 -0600

    Create and style search bar

---
+++ src/components/weather/index.jsx
@@ -0,0 +1,12 @@
+import Search from '../search';
+
+export default function Weather() {
+  return (
+    <div>
+      <Search />
+      <h1>weather</h1>
+    </div>
+  );
+}
+
+// https://www.youtube.com/watch?v=5ZdHfJVAY-s  5:27:59
