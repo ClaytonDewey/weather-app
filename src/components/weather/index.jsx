8a9317ab96938c40e9fbf7171a6476eb8f0fbeb1
ClaytonDewey <clay@dryadmedia.com>
2024-02-29 13:30:19 -0600

    Fetch weather and display
    TODO: add measurement switcher

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,12 +1,105 @@
+import { useEffect, useState } from 'react';
 import Search from '../search';
 
 export default function Weather() {
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  const KELVIN = 273.15;
+
+  async function fetchWeatherData(param) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=${apiKey}`
+      );
+
+      const data = await response.json();
+
+      if (data) {
+        setWeatherData(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  }
+
+  function handleSearch() {
+    fetchWeatherData(search);
+  }
+
+  function getCurrentDate() {
+    return new Date().toLocaleDateString('en-us', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  }
+
+  function getFahrenheit(currentTemp) {
+    const fahrenheit = Math.floor((currentTemp - KELVIN) * 1.8 + 32);
+    return fahrenheit;
+  }
+
+  function getCelcius(currentTemp) {
+    const celcius = Math.floor(currentTemp - KELVIN);
+    return celcius;
+  }
+
+  useEffect(() => {
+    fetchWeatherData('Madison');
+  }, []);
+
   return (
     <div>
-      <Search />
-      <h1>weather</h1>
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
+      {loading ? (
+        <div className='loading'>Loading...</div>
+      ) : (
+        <div>
+          <div className='city-name'>
+            <h2>
+              {weatherData?.name}, <span>{weatherData?.sys?.country}</span>
+            </h2>
+          </div>
+          <div className='date'>
+            <span>{getCurrentDate()}</span>
+          </div>
+          <div className='temp'>
+            {getFahrenheit(weatherData?.main?.temp)}&deg;F (
+            {getCelcius(weatherData?.main?.temp)}&deg;C)
+          </div>
+          <p className='description'>
+            {weatherData && weatherData.weather && weatherData.weather[0]
+              ? weatherData.weather[0].description
+              : ''}
+          </p>
+          <div className='weather-info'>
+            <div className='column'>
+              <div>
+                <p className='wind'>{weatherData?.wind?.speed}</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+            <div className='column'>
+              <div>
+                <p className='wind'>{weatherData?.main?.humidity}%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-// https://www.youtube.com/watch?v=5ZdHfJVAY-s  5:27:59
+//
